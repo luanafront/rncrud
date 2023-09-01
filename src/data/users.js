68b13eae33919a3c6752246551469f34617b5328
luanafront <luana.lopes.rodrigues1@gmail.com>
2023-09-01 13:49:25 -0300

    commit inicial

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    name: "Luana Medeiros",
+    email: "luana@gmail.com",
+    avatarUrl: "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png",
+  },
+  {
+    id: 2,
+    name: "How Aguiar",
+    email: "how@gmail.com",
+    avatarUrl: "https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_960_720.png",
+  },
+  {
+    id: 3,
+    name: "Cacau Menezes",
+    email: "cacau@gmail.com",
+    avatarUrl: "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png",
+  },
+  {
+    id: 4,
+    name: "Nick Smith",
+    email: "nick@gmail.com",
+    avatarUrl: "https://cdn.pixabay.com/photo/2016/11/01/21/11/avatar-1789663_960_720.png",
+  },
+];
