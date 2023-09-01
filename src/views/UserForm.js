12c4e7436834fcec08d0afda2f0f23566e5f4cb3
luanafront <luana.lopes.rodrigues1@gmail.com>
2023-09-01 15:24:26 -0300

    commit

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,8 +1,54 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Text, TextInput, View, StyleSheet } from "react-native";
+import { Button } from "react-native-elements";
 
-export default props => {
+export default ({route, navigation}) => {
+  const [user, setUser] = useState(route.params ? route.params : {})
+
   return (
-    <Text>UserForm</Text>
+    <View style={style.form}>
+      <Text>Name</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={name => setUser({...user, name})}
+        placeholder="Informe o Nome"
+        value={user.name}
+      />
+      <Text>Email</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={email => setUser({...user, email})}
+        placeholder="Informe o email"
+        value={user.email}
+      />
+      <Text>URL do Avatar</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+        placeholder="Informe a URL do Avatar"
+        value={user.avatarUrl}
+      />
+      <Button
+        title="Salvar"
+        onPress={() => {
+          navigation.goBack()
+        }}
+      />
+    </View>
   )
 }
+
+
+const style = StyleSheet.create({
+  form: {
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    paddingLeft: 5,
+    borderWidth: 1,
+    marginBottom: 10,
+    borderRadius: 5,
+  }
+})
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,21 +1,54 @@
-import React from "react";
-import { View, FlatList, Text} from "react-native";
-import { Avatar, ListItem } from "react-native-elements";
+import React, { useContext } from "react";
+import { View, FlatList, Text, Alert } from "react-native";
+import { Avatar, Button, Icon, ListItem } from "react-native-elements";
 
 import users from "../data/users";
+import { UsersContext } from "../context/UsersContext";
 
 export default props => {
+
+  useContext(UsersContext)
+
+  function confirmUserDeletion(user) {
+    Alert.alert("Excluir Usuário", "Deseja excluir o usuário?", [
+      {
+        text: "Sim",
+        onPress() {
+          console.warn("delete " + user.id);
+        },
+      },
+      {
+        text: "Não",
+      },
+    ]);
+  }
+
+  function getAction(user) {
+    return (
+      <>
+        <Button
+          onPress={() => props.navigation.navigate("UserForm", user)}
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+        />
+        <Button
+          onPress={() => confirmUserDeletion(user)}
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+        />
+      </>
+    );
+  }
+
   function getUserItem({ item: user }) {
     return (
-      <ListItem
-        bottomDivider
-        onPress={() => props.navigation.navigate("UserForm")}>
+      <ListItem bottomDivider>
         <Avatar source={{ uri: user.avatarUrl }} />
         <ListItem.Content>
           <ListItem.Title>{user.name}</ListItem.Title>
           <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
         </ListItem.Content>
-        <ListItem.Chevron />
+        {getAction(user)}
       </ListItem>
     );
   }
